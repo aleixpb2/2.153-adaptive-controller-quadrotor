df9ee58a0f555cefbc2500c7c1fa1afdc12909fb
Aleix Paris <aleix@mit.edu>
2019-11-23 20:51:11 -0500

    matrices are set

--- src/main.m
+++ src/main.m
@@ -1,14 +1,24 @@
 clc, clear, close all;
 
+np = 3*4;
+m = 4;
+n = np + m;  % 16
+
 % Load drone parameters
 quad = droneParams;
 
 rho = 0.2;
+max_pos = 1000;
+max_ang = 2*pi;
+max_vel = 1000;
+max_rate = 4*pi;
+max_eyI = 0.5;  % integral of tracking error
 max_states = [
-    1; 1; 1;
-    0.1; 0.1; 0.1;
-    1; 1; 1;
-    0.1; 0.1; 0.1;
+    max_pos; max_pos; max_pos;
+    max_ang; max_ang; max_ang;
+    max_vel; max_vel; max_vel;
+    max_rate; max_rate; max_rate;
+    max_eyI;max_eyI;max_eyI;max_eyI;
     ];
 max_inputs = [quad.U1_max; quad.U2_max; quad.U3_max; quad.U4_max];
 
@@ -16,7 +26,24 @@ Q = diag(1./max_states.^2);
 R = diag(1./max_inputs.^2);
 R = R.*rho;
 
-Apbar = ;
-B = ;
-Abar = [Apbar ...];
-[K,S,P] = lqr(Abar, B, Q, R)
+Ap = [
+    zeros(3,3), zeros(3,3), eye(3), zeros(3,3);
+    zeros(3,3), zeros(3,3), zeros(3,3), eye(3);
+    zeros(3,3), [0 quad.g 0; -quad.g 0 0; 0 0 0], zeros(3,3), zeros(3,3);
+    zeros(3, np)
+    ];
+
+Cp = zeros(m, np);
+Cp(1,1) = 1; Cp(2,2) = 1; Cp(3,3) = 1;
+Cp(4,6) = 1;
+
+Abar = [Ap zeros(np,m); Cp zeros(m,m)];
+
+Bp = zeros(np,m);
+Bp(9,1) = 1/quad.M;
+Bp(10,2) = quad.d/quad.Ix;
+Bp(11,3) = quad.d/quad.Iy;
+Bp(12,4) = 1/quad.Iz;
+B = [Bp; zeros(m,m)];
+
+%[K,S,P] = lqr(Abar, B, Q, R)
