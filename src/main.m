f9e45be8cf677d7db062deff9392e895efd5e3c6
Aleix Paris <aleix@mit.edu>
2019-11-24 02:14:25 -0500

    simulink

--- src/main.m
+++ src/main.m
@@ -7,25 +7,8 @@ n = np + m;  % 16
 % Load drone parameters
 quad = droneParams;
 
-rho = 0.2;
-max_pos = 1000;
-max_ang = 2*pi;
-max_vel = 1000;
-max_rate = 4*pi;
-max_eyI = 0.5;  % integral of tracking error
-max_states = [
-    max_pos; max_pos; max_pos;
-    max_ang; max_ang; max_ang;
-    max_vel; max_vel; max_vel;
-    max_rate; max_rate; max_rate;
-    max_eyI;max_eyI;max_eyI;max_eyI;
-    ];
-max_inputs = [quad.U1_max; quad.U2_max; quad.U3_max; quad.U4_max];
-
-Q = diag(1./max_states.^2);
-R = diag(1./max_inputs.^2);
-R = R.*rho;
-
+% Matrix definitions
+Lambda = eye(m);  % unknown pos. def. matrix
 Ap = [
     zeros(3,3), zeros(3,3), eye(3), zeros(3,3);
     zeros(3,3), zeros(3,3), zeros(3,3), eye(3);
@@ -45,5 +28,28 @@ Bp(10,2) = quad.d/quad.Ix;
 Bp(11,3) = quad.d/quad.Iy;
 Bp(12,4) = 1/quad.Iz;
 B = [Bp; zeros(m,m)];
+Bc = [zeros(np,m); -eye(m)];
 
-%[K,S,P] = lqr(Abar, B, Q, R)
+% Load LQR parameters
+rho = 0.2;
+max_pos = 1000;
+max_ang = 2*pi;
+max_vel = 1000;
+max_rate = 4*pi;
+max_eyI = 0.5;  % integral of tracking error
+max_states = [
+    max_pos; max_pos; max_pos;
+    max_ang; max_ang; max_ang;
+    max_vel; max_vel; max_vel;
+    max_rate; max_rate; max_rate;
+    max_eyI;max_eyI;max_eyI;max_eyI;
+    ];
+max_inputs = [quad.U1_max; quad.U2_max; quad.U3_max; quad.U4_max];
+
+Q = diag(1./max_states.^2);
+R = diag(1./max_inputs.^2);
+R = R.*rho;
+
+[K,S,P] = lqr(Abar, B, Q, R);
+%K(K<1e-5) = 0
+Kbl = -K;
